66a950967f571fe868b58801b7ae2a8e65dd4f5d
KrissVvalance <98370315+KrissVvalance@users.noreply.github.com>
2022-01-25 10:24:27 +0700

    Add files via upload

---
+++ scripts/form-carousel_64b0.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'ส่งอีเมลล์ติดต่อกับบริษัท',
+    'email_message' => '',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => '',
+    'to' => 'Viorabio@hotmail.com'
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'name' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
